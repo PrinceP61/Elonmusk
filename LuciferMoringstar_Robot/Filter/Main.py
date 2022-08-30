c46c5b4523f94426c18af926944b37c1245973b6
BELUGA_XD <96910602+belugaop@users.noreply.github.com>
2022-08-30 21:40:42 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -127,10 +127,9 @@ async def group(client, message):
                 file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 url = https://api.shareus.in/shortLink?token=xGzniHOkadfY4aP4HKbnGxHGGkv1&format=json&link=https://telegram.dog/{nyva}?start=pr0fess0r_99_-_-_-_{file_id}
-
-res = requests.get(url).json()
-short_link = res["shortlink"] 
-btn.append([InlineKeyboardButton(text=f"{filename}", url=short_link)] )
+                res = requests.get(url).json()
+                short_link = res["shortlink"] 
+                btn.append([InlineKeyboardButton(text=f"{filename}", url=short_link)] )
         else:
             return
         if not btn:
